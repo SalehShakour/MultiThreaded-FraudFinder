dc780eb9a1c7e38543a7523326b5d4217e3ebdb7
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-15 11:52:02 +0330

    synchronized CS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
@@ -30,27 +26,34 @@ class Main {
         CSVReader accountCsv = new CSVReader(accountsReader);
         String[] nextTransaction;
         String[] nextAccount;
-
         while (true) {
             try {
-                if ((nextTransaction = transactionCsv.readNext()) == null || (nextAccount = accountCsv.readNext()) == null) break;
+                if ((nextAccount = accountCsv.readNext()) == null) break;
             } catch (IOException | CsvValidationException e) {
                 throw new RuntimeException(e);
             }
-
-            List<String> tempTransaction = Arrays.stream(nextTransaction).toList();
             List<String> tempAccount = Arrays.stream(nextAccount).toList();
 
             int accountID = Integer.parseInt(tempAccount.get(0).replace("\uFEFF", ""));
             String accountName = tempAccount.get(1).replace("\uFEFF", "");
             long balance = Long.parseLong(tempAccount.get(2).replace(",", ""));
             accounts.put(accountID,new Account(accountID,accountName,balance));
-
+        }
+        while (true) {
+            try {
+                if ((nextTransaction = transactionCsv.readNext()) == null) break;
+            } catch (IOException | CsvValidationException e) {
+                throw new RuntimeException(e);
+            }
+            List<String> tempTransaction = Arrays.stream(nextTransaction).toList();
             int srcID = Integer.parseInt(tempTransaction.get(0).replace("\uFEFF", ""));
             int desID = Integer.parseInt(tempTransaction.get(1).replace("\uFEFF", ""));
             long amount = Long.parseLong(tempTransaction.get(2).replace(",", ""));
             transactions.add(new Transaction(accounts.get(srcID),accounts.get(desID),amount));
         }
+
+
+        ThreadManager threadManager = new ThreadManager(transactions,accounts);
 
 
 
--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,11 @@
-public record Transaction(Account sourceID, Account destinationID, long amount) {
+public record Transaction(Account source, Account destination, long amount) {
+    public void apply(){
+        synchronized (source){
+            source.setReceivable(source.getReceivable() + amount);
+        }
+        synchronized (destination){
+            destination.setPayable(destination.getPayable() + amount);
+        }
+
+    }
 }
