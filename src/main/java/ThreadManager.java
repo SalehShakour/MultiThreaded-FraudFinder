06ab9bf53a700df5308a86087a2ba13ff82fecfd
Saleh Shakour <saleh.shakour2000@gmail.com>
2023-08-15 11:52:35 +0330

    run tasks on thread

--- src/main/java/ThreadManager.java
+++ src/main/java/ThreadManager.java
@@ -1,12 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class ThreadManager {
     private final List<Transaction> transactionList;
-    private final List<Account> accountList;
+    private final HashMap<Integer, Account> accountList;
 
-    public ThreadManager(List<Transaction> transactionList, List<Account> accountList) {
+    public ThreadManager(List<Transaction> transactionList, HashMap<Integer, Account> accountList) {
         this.transactionList = transactionList;
         this.accountList = accountList;
+        start();
     }
+    public void start(){
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<Runnable> tasks = transactionList.stream()
+                .map(transaction -> (Runnable) transaction::apply)
+                .toList();
+        for (Runnable task : tasks) {
+            executor.execute(task);
+        }
+        for (Account account:accountList.values()){
+            System.out.println("ID: " + account.getAccountID());
+            System.out.println("balance before changes: " + account.getBalance());
+            System.out.println("payable : " + account.getPayable());
+            System.out.println("receivable : " + account.getReceivable());
+            long newBalance = account.getBalance() + account.getPayable() - account.getReceivable();
+            System.out.println("new balance : " + newBalance + "\n" + " -------------- ");
+        }
+        executor.shutdown();
+    }
+
+
+//    public int getOptimalThreadNumber(){
+//        int availableCores = Runtime.getRuntime().availableProcessors();
+//        double waitTime = ...; // time a task spends waiting in the queue
+//        double serviceTime = ...; // time it takes for a task to be executed
+//        int optimalThreads = (int) (availableCores * (1 + waitTime / serviceTime));
+//
+//    }
 
 }
